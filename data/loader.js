f53a945222f029366b3e004fc92a33652baf77b4
batoceraonline <batoceraonline@gmail.com>
2025-04-27 22:13:21 -0300

    Update loader.js

--- data/loader.js
+++ data/loader.js
@@ -192,7 +192,6 @@ if (lista) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Detectar iOS
     function isIOS() {
         return /iPhone|iPad|iPod/i.test(navigator.userAgent);
     }
@@ -200,15 +199,7 @@ document.addEventListener('DOMContentLoaded', function() {
     if (isIOS()) {
         var gameDiv = document.getElementById('game');
         if (gameDiv) {
-            // Cria o novo div
-            var game2Div = document.createElement('div');
-            game2Div.id = 'game2';
-
-            // Insere o game2 antes do game
-            gameDiv.parentNode.insertBefore(game2Div, gameDiv);
-
-            // Oculta o game original
-            gameDiv.style.display = 'none';
+            gameDiv.id = 'game2'; // Muda o id de 'game' para 'game2'
         }
     }
 });
